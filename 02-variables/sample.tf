bb88dde99f006b12e2855ea325d5518bded406d2
windows <veeranki2014@gmail.com>
2026-04-20 00:03:52 -0500

    learn-roboshop concepts

--- 02-variables/sample.tf
+++ 02-variables/sample.tf
@@ -14,3 +14,58 @@ output "x11" {
 
 # x1 = abc   # shell
 # x1: abc    # Ansible
+
+# Data Types
+# String, need to be given in double quotes
+# number, No need of quote.
+# Boolean, No need to quote.
+
+variable "string1" {
+  default = "abc"
+}
+
+variable "number1" {
+  default = "abc"
+}
+
+variable "number2" {
+  default = "abc"
+}
+
+# true or false is supported ( Ansible supports Yes or No)
+variable "boolean1" {
+  default = true
+}
+
+# Variable Types
+# Plain
+# List
+# Map / Dict
+
+variable "plain1" {
+  default = "abc"
+}
+
+variable "list1" {
+  default = ["abc", 123, true]
+}
+
+variable "map1" {
+  default = {
+    name = "devops"
+    skill = "awsdevops"
+  }
+}
+
+# How retrieve values
+output "list1_value1" {
+  value = var.list1[0]
+}
+
+output "map1_name" {
+  value = var.map1["name"]
+}
+
+
+
+
