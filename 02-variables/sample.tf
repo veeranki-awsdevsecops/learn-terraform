a3510188f5b9d40f2f2d561964603c9395cf586e
windows <veeranki2014@gmail.com>
2026-04-19 23:49:16 -0500

    learn-roboshop concepts

--- 02-variables/sample.tf
+++ 02-variables/sample.tf
@@ -1,14 +1,16 @@
-# x1 = abc   #shell
-# x1: abc    #Ansible
+# x1 = abc   # shell
+# x1: abc    # Ansible
 
 variable "x1" {
   default = "abc"
 }
 
 output "x1" {
-  value = var.x1                                  # variable var.x1 for the direct variable usage.
+  # variable var.x1 for the direct variable usage.
+  value = var.x1
 }
 
 output "x11" {
-  value = "Value of the x1 is ${var.x1}"          # Variable var.x1 is with combination of a string then ${}
+  # Variable var.x1 is with combination of a string then ${}
+    value = "Value of the x1 is ${var.x1}"
 }
